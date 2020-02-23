338f302123ae39d8afb9507fa0f9bca111f672a0
Dilshad Mustafa <mdilshad2016@yahoo.com>
2020-02-23 09:57:31 +0530

    doc updates

--- lispcmd/src/lispcmd/LispCmd.java
+++ lispcmd/src/lispcmd/LispCmd.java
@@ -2,8 +2,11 @@ package lispcmd;
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.UUID;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
@@ -31,7 +34,15 @@ public class LispCmd {
 		// catch interupted exceptio and exit app
 	}
 	
-	public static void main(String args[]) throws IOException, InterruptedException {
+	static String replaceAll(String s) {
+		s = s.replaceAll("\n", " ");
+		s = s.replaceAll("\r", " ");
+		
+		return s;
+		
+	}
+	
+	public static void main(String args[]) {
 		
         String ready = "DILMUS-CAI-ready";
 		String ok1 = "DILMUS-CAI-ok1";
@@ -43,38 +54,73 @@ public class LispCmd {
 	    scriptManager.registerEngineExtension("lisp", new AbclScriptEngineFactory());
 	    ScriptEngine lispEngine = scriptManager.getEngineByExtension("lisp");	
 		
-		try {
-		System.out.println(ready);
+	    UUID uuid = UUID.randomUUID();
+	    int uuidlen = uuid.toString().length();
+
+	    System.out.println(ready);
+
 		while(true) {
+	    
+	    
+		try {
+		//DEBUG SECTION-------------------
 		str = br.readLine();
-		
+		//str = "adb19b1d-0ad0-4766-8f51-12451b842e57 (defmacro slow-compiling-macro (arg) (dotimes (i 1000000) (incf i)) `(print ,arg)) (slow-compiling-macro 42)";
+		//--------------------------------
 		System.out.println(ok1 + " " + str);
 		
-		//---------------------------------------------
-		// processing section
+		//PROCESSING SECTION---------------
 		// do not output to stdout
-		Object obj = lispEngine.eval(str);
-		System.out.println(ok2 + " " + obj);
 		
-		//------------------------------------------------
+		//4e177d29-bcf4-4a32-9c1f-e81dc584e06e
+		String suuid = str.substring(0, uuidlen);
+		String str2 = str.substring(uuidlen, str.length());
 		
-		//Thread.sleep(7000);
-		//System.out.println(ok2);
+		File f = new File("/home/anees/lisptmp/res_" + suuid + ".txt");
+		FileWriter fw = new FileWriter(f);
+		
+		lispEngine.getContext().setWriter(fw);
+		Object obj = lispEngine.eval(str2);
+		
+		fw.close();
+		//---------------------------------
+		
+		//DEBUG SECTION-------------
+		//lispEngine.eval(str2);
+		//System.out.println("actual output  : " + obj.toString());
+		//--------------------------
+		String s = obj.toString();
+		s = replaceAll(s);
+		System.out.println(ok2 + " " + s);
 		System.out.println(ready);
 		}
-		} 
 		catch (ScriptException e) {
-			e.printStackTrace();
-		}		
+			String ex = replaceAll(e.toString());
+			System.out.println(ok2 + " " + ex);
+			System.out.println(ready);
 
+		}
+//		catch (InterruptedException e) {
+//			System.exit(0);
+//		}
+		catch (Exception e) {
+			String ex = replaceAll(e.toString());
+			System.out.println(ok2 + " " + ex);
+			System.out.println(ready);
+			
+		}
+		catch (Error e) {
+			String ex = replaceAll(e.toString());
+			System.out.println(ok2 + " " + ex);
+			System.out.println(ready);
+			
+		}
 		// catch interupted exception and exit app
-	}
-	
-	
-	
-	
-	
-	
-	
+		//DEBUG SECTION-------------
+		//break;
+		//--------------------------
+		}
+		
+	} 
 	
 }
--- lispcmd/src/lispcmd/LispCmdClient.java
+++ lispcmd/src/lispcmd/LispCmdClient.java
@@ -1,4 +1,4 @@
-package com.dilmus.dilshad.lispcmd;
+package lispcmd;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -107,16 +107,93 @@ public class LispCmdClient {
 	}
 	
 	public static void main(String args[]) throws InterruptedException, ExecutionException, TimeoutException {
-			
+		
+		
 	try {
 		String actoruuid = "123f0bcb-d791-4909-bd16-859a96ce9c4f";
 		String lisprunfile = createLispRunFile(actoruuid);
 		Process p2 = execLispRunFile(lisprunfile);
-		waitAndRemove(p2);         
+		waitAndRemove(p2);
+		
+		long millis = System.currentTimeMillis();
+		ProcessBuilder pb = new ProcessBuilder("java", "-cp", 
+        		 "/home/anees/Downloads/abcl-bin-1.6.0/abcl.jar:/home/anees/allprjs/CaretakerAI/lispcmd/bin/",
+        		 "lispcmd.LispCmd");       
+         pb.redirectErrorStream(true);
+         pb.environment().put("user.dir", "/home/anees/allprjs/CaretakerAI/lispcmd/bin/");
+         //System.out.println("p1");
+         final Process p=pb.start();
+         System.out.println("process start time taken : " + (System.currentTimeMillis() - millis));
+         //System.out.println("p2");
+         //Thread.sleep(10000);
+         
+         BufferedReader br=new BufferedReader(new InputStreamReader(p.getInputStream()));
+         BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
+
+         //UUID uuid = UUID.randomUUID();
+         String uuid = "be6f0bcb-d791-4909-bd16-859a96ce9c4f";
+         millis = System.currentTimeMillis();
+         sendRequest("(+ 23 45)", uuid.toString(), p, br, bw);
+         receiveResponse(p, br, bw);
+         System.out.println("req-res time taken : " + (System.currentTimeMillis() - millis));
+
+         millis = System.currentTimeMillis();
+         sendRequest("(+ 100 1)", uuid.toString(), p, br, bw);
+         receiveResponse(p, br, bw);
+         System.out.println("req-res time taken : " + (System.currentTimeMillis() - millis));
+         
+         millis = System.currentTimeMillis();
+         sendRequest("(defmacro slow-compiling-macro (arg) (dotimes (i 1000000) (incf i)) `(print ,arg)) (slow-compiling-macro 42)", uuid.toString(), p, br, bw);
+         receiveResponse(p, br, bw);
+         System.out.println("req-res time taken : " + (System.currentTimeMillis() - millis));
+        
+         
+         /*
+         String line = null;
+         
+         line=br.readLine();
+         System.out.println(line);
+         
+         //System.out.println("p4");
+         String str2 = "testing";
+         bw.write(str2);
+         bw.newLine();
+         bw.flush();
+         //System.out.println("p3");
+         
+         line=br.readLine();
+         System.out.println(line);
+         line=br.readLine();
+         System.out.println(line);
+
+         //System.out.println("p5");
+         */
+         /*
+         Thread.sleep(10000);
+         while((line=br.readLine())!=null){
+             System.out.println(line);
+          }                 
+          */
+         
+         System.out.println(p.isAlive());
+         
+ 
+         
+         
       } catch (Exception ex) {
          System.out.println(ex);
       }	
-				
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 	
 }
