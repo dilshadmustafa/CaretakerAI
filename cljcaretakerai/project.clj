9bf03201f068754a06dc3da7eb863c6a1ac8568d
Dilshad Mustafa <mdilshad2016@yahoo.com>
2020-09-16 19:35:24 +0530

    updates

--- cljcaretakerai/project.clj
+++ cljcaretakerai/project.clj
@@ -1,9 +1,10 @@
 
-(defproject ex1 "0.1.0-SNAPSHOT"
+(defproject cljcaretakerai "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure.gaverhae/okku "0.1.5"]
-                 [com.couchbase.client/java-client "3.0.8"]]
-  :repl-options {:init-ns ex1.core})
+                 [com.couchbase.client/java-client "3.0.8"]
+                 [org.clojure/tools.namespace "1.0.0"]]
+  :repl-options {:init-ns cljcaretakerai.core})
 
